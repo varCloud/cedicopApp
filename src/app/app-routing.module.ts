210e5209a93e295dcb37b8b66c4bf1feead0be7e
Victor Adrian Reyes <victor.reyez@cmv.mx>
2020-02-14 15:40:51 -0600

    se agrega android

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
   { path: 'principal', loadChildren: './principal/principal.module#PrincipalPageModule' },
   { path: 'principal/:id', loadChildren: './acuerdos/acuerdos.module#AcuerdosPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  //{ path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'registro', loadChildren: './registro/registro.module#RegistroPageModule' },
   { path: 'acuerdos', loadChildren: './acuerdos/acuerdos.module#AcuerdosPageModule' },
   { path: 'acuerdos-detalle', loadChildren: './acuerdos-detalle/acuerdos-detalle.module#AcuerdosDetallePageModule' },
